fbdd03dfdd129aa701bb93cc3f67eb9110ae3326
xMohnad <mohnad00967@gmail.com>
2026-03-31 16:03:59 +0000

    refactor(handlers): remove inline query handler

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -2,6 +2,5 @@ from .archive import router as archive
 from .bot import router as bot
 from .channel import router as channel
 from .commands import router as commands
-from .inline_query import router as inline_query
 
-routers = [channel, bot, archive, commands, inline_query]
+routers = [channel, bot, archive, commands]
